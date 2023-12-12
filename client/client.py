0ebb4ced79b2815f13cd22c7ca4146d04250085f
lisazhanglii <2010295336@qq.com>
2023-12-11 22:00:28 -0500

    minor change

--- client/client.py
+++ client/client.py
@@ -4,6 +4,8 @@ import server.post_pb2_grpc
 import server.comment_pb2_grpc
 import server.subreddit_pb2_grpc
 import server.update_monitor_pb2_grpc
+import server.post_pb2 as post_pb2
+import server.comment_pb2 as comment_pb2
 import time
 
 class RedditClient:
@@ -29,5 +31,22 @@ class RedditClient:
             # In a real application, this would be triggered by some condition
             yield server.update_monitor_pb2.UpdateRequest(id=post_id, type=server.update_monitor_pb2.UpdateRequest.POST)
             time.sleep(1)  # Simulate waiting for an update
-# Example usage
-client = RedditClient()
+    def retrieve_post(self, title):
+        """Retrieve a post by its title."""
+        return self.post_stub.RetrievePost(post_pb2.PostRequest(title=title))
+
+    def retrieve_top_comments(self, post_id, top_n=1):
+        """Retrieve the top N comments for a post."""
+        return self.comment_stub.RetrieveTopComments(comment_pb2.TopCommentsRequest(post_id=post_id, top_n=top_n))
+
+    def expand_comment_branch(self, comment_id, max_depth=1):
+        """Expand the comment branch to retrieve replies."""
+        return self.comment_stub.ExpandCommentBranch(comment_pb2.CommentBranchRequest(comment_id=comment_id, max_depth=max_depth))
+
+if __name__ == '__main__':
+    client = RedditClient()
+    post = client.retrieve_post('xxpost')
+    top_comments = client.retrieve_top_comments(post_id=post.post_id, top_n=1)
+    if top_comments.comments:
+        most_upvoted_comment = top_comments.comments[0]
+        expanded_comment = client.expand_comment_branch(comment_id=most_upvoted_comment.comment_id, max_depth=1)
--- client/client_utilities.py
+++ client/client_utilities.py
@@ -6,22 +6,19 @@ import server.comment_pb2 as comment_pb2
 def retrieve_and_expand_post_comments(client, post_title):
     """
     Retrieves a post, the most upvoted comments under it, and expands the most upvoted comment.
-    :param client: Instance of RedditClient
-    :param post_title: The title of the post
-    :return: The most upvoted reply under the most upvoted comment or None
     """
-    post = client.post_stub.RetrievePost(post_pb2.PostRequest(title=post_title))
-    if not post:
+    post_response = client.retrieve_post(post_title)
+    if post_response is None or not hasattr(post_response, 'post'):
         return None
 
-    top_comments = client.comment_stub.RetrieveTopComments(comment_pb2.TopCommentsRequest(post_id=post.post_id, top_n=1))
-    if not top_comments.comments:
+    post = post_response.post
+    top_comments_response = client.retrieve_top_comments(post.post_id, top_n=1)
+    if not top_comments_response.comments:
         return None
 
-    most_upvoted_comment = top_comments.comments[0]
-    expanded_comments = client.comment_stub.ExpandCommentBranch(comment_pb2.CommentBranchRequest(comment_id=most_upvoted_comment.comment_id, max_depth=1))
-
-    if expanded_comments and expanded_comments.comments:
-        return expanded_comments.comments[0].text
-    else:
+    most_upvoted_comment = top_comments_response.comments[0]
+    expanded_comments_response = client.expand_comment_branch(most_upvoted_comment.comment_id, max_depth=1)
+    if not expanded_comments_response.comments or not expanded_comments_response.comments[0].replies:
         return None
+
+    return expanded_comments_response.comments[0].replies[0].text
